f844dd2ef5528fb6d72856c51d5dc520610de3b6
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-12 06:51:04 +0700

    update

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './user/user.service';
+import { CoModule } from './user/co/co.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [CoModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
