7421b3d7e914497fc604d2aac4037e2c05248bd1
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-13 00:24:42 +0700

    add module product

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { CoModule } from './user/co/co.module';
 import { UserModule } from './user/user.module';
+import { ProductController } from './product/product.controller';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [CoModule, UserModule],
-  controllers: [AppController],
+  imports: [CoModule, UserModule, ProductModule],
+  controllers: [AppController, ProductController],
   providers: [AppService, UserService],
 })
 export class AppModule {}
