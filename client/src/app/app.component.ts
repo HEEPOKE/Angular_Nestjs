53edca39dcf905e9aeda02a2fe71354d4b9ce58c
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-11 16:03:48 +0700

    Update app.component.ts

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgbDropdownModule],
+  imports: [RouterModule, CommonModule, NgbDropdownModule],
   templateUrl: './app.component.html',
-  styleUrls: ['../styles.css']
+  styleUrls: ['../styles.css'],
 })
 export class AppComponent {
   title = 'client';
